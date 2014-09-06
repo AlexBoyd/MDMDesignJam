0302f6b8c065f8b861fa91588d81583b2eb4b6a0
AlexBoyd <alexander.ray.boyd@gmail.com>
2014-09-06 14:34:20 -0700

    more work on attack tuning

--- DesignJam/Assets/Attack.cs
+++ DesignJam/Assets/Attack.cs
@@ -4,16 +4,32 @@ using System.Collections;
 public class Attack : MonoBehaviour {
 
 	public ParticleSystem fireCone;
-	public float fireballForce = 10;
-	// Use this for initialization
-	void Start () 
+	public float fireballForce = 10f;
+	public float fireballLength = 0.2f;
+	public float fireballCooldown = 0.4f;
+
+	private bool isShooting = false;
+
+
+	void Start()
 	{
-	
-	}
-	
+		fireCone.enableEmission = false;
+		}
 	// Update is called once per frame
 	void Update () 
 	{
-		fireCone.enableEmission = Input.GetKey(KeyCode.Space);
+		if (!isShooting && Input.GetKeyUp(KeyCode.Space)) {
+			StartCoroutine(FireCooldown());		
+		}
+	}
+
+	private IEnumerator FireCooldown()
+	{
+		isShooting = true;
+		fireCone.enableEmission = true;
+		yield return new WaitForSeconds(fireballLength);
+		fireCone.enableEmission = false;
+		yield return new WaitForSeconds(fireballCooldown);
+		isShooting = false;
 	}
 }
