0e7d438d91b90c53af6208377023c3d367ff25ea
AlexBoyd <alexander.ray.boyd@gmail.com>
2014-09-06 11:15:40 -0700

    adding in attack code and more oculus sdk stuff

---
+++ DesignJam/Assets/Attack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+	public GameObject fireballPrefab;
+	public float fireballForce = 10;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			GameObject fireball = GameObject.Instantiate(fireballPrefab, transform.position, transform.rotation) as GameObject;
+			fireball.rigidbody.AddForce(transform.forward * fireballForce);
+		}
+	}
+}
