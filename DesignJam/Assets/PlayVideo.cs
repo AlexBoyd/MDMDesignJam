7e149e2551e4e1556fe9227859227d12f95b56b4
AlexBoyd <alexander.ray.boyd@gmail.com>
2014-09-07 11:56:49 -0700

    video playback

---
+++ DesignJam/Assets/PlayVideo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideo : MonoBehaviour {
+
+	public MovieTexture VideoFile = null;
+	// Use this for initialization
+	void OnGUI () 
+	{
+
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),VideoFile,ScaleMode.StretchToFill);
+	}
+
+	void Awake ()
+	{
+		VideoFile.Play ();
+		audio.clip = VideoFile.audioClip;
+		audio.Play ();
+		}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!VideoFile.isPlaying) {
+			Application.LoadLevel(1);		
+		}
+	}
+}
