b9117e835a57455cf89b9e63ff30576636ea9186
marisbr <marina_lucio@thecdm.ca>
2014-09-07 11:38:28 -0700

    final scene

---
+++ DesignJam/Assets/Plugins/Pixelplacement/iTween/playerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class playerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//iTween.MoveTo(gameObject, iTween.Hash ("path", iTweenPath.GetPath("playerMovement"), "time", 5));
+	
+	}
+
+}
---
+++ DesignJam/Assets/playerMovement2.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement2 : MonoBehaviour {
+
+	public void Start()
+	{
+		iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath ("playerMovement"), "time", 1000));
+	}
+}
