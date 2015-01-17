2580604a1aa3c3dc0cd2907f3c40c12fb8193e54
AlexBoyd <alexander.ray.boyd@gmail.com>
2015-01-17 13:41:53 -0800

    Adding debug informaton

--- DesignJam/Assets/playerMovement2.cs
+++ DesignJam/Assets/playerMovement2.cs
@@ -8,5 +8,6 @@ public class playerMovement2 : MonoBehaviour {
 	public void Start()
 	{
 		iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath ("playerMovement"), "time", time));
+		Debug.Log ("Debug");
 	}
 }
